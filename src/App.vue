b38e5719441d8999c481a6c71219b0c51cc894aa
russellshire <russell.shire@gmail.com>
2023-12-26 20:36:48 +0000

    Add basic format of Fynn's first button

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <script setup>
+  import { ref } from 'vue';
+  const buttonText = ref('click me')
+  const clickCount = ref(0)
+  const messageContent = ref('')
 
+  const buttonClicked = () => {
+    clickCount = clickCount + 1;
+
+    
+  }
 </script>
 
 <template>
-  <div>hello</div>
+  <button @click="buttonClicked">{{ buttonText }}</button> 
+  <div v-if="messageContent" class="message">{{messageContent}}</div>
 </template>
 
 <style scoped>
+.message {
+  border: 1px black solid;
+  padding: 10px;
+  margin-top: 20px;
+  border-radius: 10px;
+  width: fit-content;
+  background-color: gold;
+}
 </style>
